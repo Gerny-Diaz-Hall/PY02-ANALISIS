c752489ad8eb66ad79907d0cfd6261fce818cfac
SidneySJk <125586766+SidneySJk@users.noreply.github.com>
2025-05-31 23:36:57 -0600

    Add files via upload
    Inicialization of the class Consult, including objets

---
+++ Consult.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Consult
+{
+    private Array[] Specialities = Array.Empty<Array>();
+    public Consult()
+    {
+        string State = "Inactive";
+        Array value = [];
+        Array hour = value;
+    }
+}
