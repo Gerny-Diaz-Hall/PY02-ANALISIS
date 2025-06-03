8a32d6ddc056f2510e674e281dc2b6852fceb35c
SidneySJk <125586766+SidneySJk@users.noreply.github.com>
2025-06-03 01:32:20 -0600

    Add files via upload
    Inicialitation of frontend of the class Consult

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;  // ¡Esta línea es la que faltaba!
+using Avalonia.Threading;
+using System;
+
+namespace AvaloniaApplication1;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+}
