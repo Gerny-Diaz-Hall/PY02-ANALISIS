67c5406608dbc11c89ddd7f33b1bd9026f00d5d9
SidneySJk <125586766+SidneySJk@users.noreply.github.com>
2025-06-07 13:11:42 -0600

    Update and rename Consult.cs to PY02/Office.cs
    Actualization of the class office, name of the file changed and the boolean status "Open" added

---
+++ PY02/Office.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Office
+{
+    public int Id { get; set; } = 0;
+    public List<Specialties> Specialties { get; set; } = new List<Specialty>();
+    public bool Open { get; set; } = true;
+
+    public Office(int id, List<string> Schedule, List<string> Specialties, bool Open)
+    {
+        Id = id+1;
+        Schedule = Schedule;
+        Specialties = Specialties;
+        Open = Open;
+    }
+
+    public bool changeStatus()
+    {
+        if Open == true {
+            Open == false;
+        }
+        else
+            {
+                Open == true;
+            }
+    }
+
+    public bool getStatus()
+    {
+        return Open
+    }
+}
