dafe41e0946ff725c1fa44eaafb595a7706b4a3a
Gerny Mcdiell Díaz Hall <gmdh95@gmail.com>
2025-06-07 15:20:12 -0600

    Arreglo de office Sidney

--- PY02/Office.cs
+++ PY02/Office.cs
@@ -1,33 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// PY02/Office.cs CORREGIDO
+
 using System;
+using System.Collections.Generic; // Asegúrate de tener este 'using' para List<T>
 
-public class Office
-{
-    public int Id { get; set; } = 0;
-    public List<Specialties> Specialties { get; set; } = new List<Specialty>();
-    public bool Open { get; set; } = true;
+// Probablemente querías una clase 'Specialty'. Si no, debes usar 'string'
+// Por ahora, lo cambiaré a una lista de strings para que compile.
+public class Specialty { /* ... tus propiedades para la especialidad ... */ }
 
-    public Office(int id, List<string> Schedule, List<string> Specialties, bool Open)
-    {
-        Id = id+1;
-        Schedule = Schedule;
-        Specialties = Specialties;
-        Open = Open;
+public class Office {
+    public int Id { get; set; }
+
+    // CORRECCIÓN 1: La lista debe ser de un solo tipo, por ejemplo 'Specialty' o 'string'.
+    // Si no tienes una clase 'Specialty', puedes usar 'string' temporalmente.
+    public List<string> Specialties { get; set; }
+    public bool Open { get; set; }
+
+    public Office(int id, List<string> schedule, List<string> specialties, bool open) {
+        Id = id + 1;
+        Specialties = specialties;
+        // No tiene sentido pasar 'schedule' y 'open' si no hay dónde guardarlos.
+        // Quizás necesites más propiedades en la clase Office.
+        Open = open;
     }
 
-    public bool changeStatus()
-    {
-        if Open == true {
-            Open == false;
+    public bool changeStatus() {
+        // CORRECCIÓN 2 y 3: Se usan paréntesis y el operador de asignación (=)
+        if (Open) // if (Open) es una forma corta de if (Open == true)
+        {
+            Open = false; // Asignación para cambiar el valor
+        } else {
+            Open = true; // Asignación para cambiar el valor
         }
-        else
-            {
-                Open == true;
-            }
+
+        // CORRECCIÓN 4: Devuelve el nuevo estado
+        return Open;
     }
 
-    public bool getStatus()
-    {
-        return Open
+    public bool getStatus() {
+        // CORRECCIÓN 5: Se añade el punto y coma
+        return Open;
     }
 }
